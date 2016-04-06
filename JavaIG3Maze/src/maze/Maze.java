c870184897743fd0151250253619480bb163b561
Tombean <tom.somervilleroberts@gmail.com>
2016-04-06 16:32:08 +0200

    Init

---
+++ JavaIG3Maze/src/maze/Maze.java
@@ -0,0 +1,17 @@
+package maze;
+
+import graph.Edge;
+import graph.Graph;
+import graph.Vertex;
+import graphImpl.AdjMatric;
+
+public class Maze extends AdjMatric {
+
+	public Maze(int n, boolean directed) {
+		super(n, directed);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
